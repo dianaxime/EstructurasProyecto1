2b83676fede3e8e6270cab0328faf3c43ff72134
dianaxime <35496688+dianaxime@users.noreply.github.com>
2019-03-08 23:38:26 -0600

    Add files via upload

--- Analizador.java
+++ Analizador.java
@@ -5,10 +5,34 @@
  */
 package proyecto1;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Vector;
+
 /**
  *
  * @author DIANA
  */
 public class Analizador {
+    
+    private String miprograma ="";
+    private Vector<String> simbolos = new Vector<>();
+    
+    
+    public Analizador(InputStream clasificar) throws IOException{
+        byte[] bytes = new byte[1024];
+        while (clasificar.available()>0){
+            clasificar.read(bytes, 0, 1024);
+            miprograma = miprograma.concat(new String(bytes)).trim().toUpperCase();
+        }
+        simbolos = simbologia(miprograma);
+    }
+    public Analizador (String clasificar){
+        miprograma=clasificar;
+        simbolos = simbologia(miprograma);
+    }
+    public Vector <String> obtenersimbolos(){
+        return simbolos;
+    }
     
 }
